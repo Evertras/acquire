7a3eea009d3475b826cee3835cc3d8a89208dff7
Brandon Fulljames <bfulljames@blizzard.com>
2017-01-11 12:20:10 -0800

    Working merge state, could be a little cleaner

--- lib/stateMerge.go
+++ lib/stateMerge.go
@@ -12,5 +12,55 @@ func NewStateMerge(triggeringPiece Piece) StateMerge {
 
 // Do chooses which hotel will obtain the other(s) and proceeds to the next step
 func (s StateMerge) Do(g *Game) State {
+	neighbors := g.Board.GetNeighbors(s.TriggeringPiece)
+
+	biggestHotelSize := 0
+	contenders := make([]Hotel, 4)[:0]
+
+	for _, n := range neighbors {
+		h := g.Board.Tiles[n.Row][n.Col]
+		c := g.CurrentChainSizes[h]
+
+		if c >= biggestHotelSize {
+			biggestHotelSize = g.CurrentChainSizes[h]
+		}
+
+		if h != HotelEmpty && h != HotelNeutral {
+			contenders = append(contenders, h)
+		}
+	}
+
+	biggest := make([]Hotel, 4)[:0]
+
+	for _, h := range contenders {
+		if g.CurrentChainSizes[h] == biggestHotelSize {
+			present := false
+			for _, b := range biggest {
+				if b == h {
+					present = true
+				}
+			}
+
+			if !present {
+				biggest = append(biggest, h)
+			}
+		}
+	}
+
+	winner := g.Players[g.CurrentPlayerIndex].Merge(g, biggest)
+
+	defunctValues := make(map[Hotel]HotelWorth)
+
+	for _, h := range contenders {
+		if h != winner {
+			defunctValues[h] = g.GetWorth(h)
+			g.CurrentChainSizes[h] = 0
+		}
+	}
+
+	grewBy := g.Board.Fill(s.TriggeringPiece, winner)
+
+	g.CurrentChainSizes[winner] += grewBy
+
 	return nil
 }
--- lib/stateMerge_test.go
+++ lib/stateMerge_test.go
@@ -1,8 +1,11 @@
 package acquire
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestStateMergeFillsInCorrectly(t *testing.T) {
+func TestStateMergeFillsCorrectly(t *testing.T) {
 	r, ps := genGameParams()
 	g := NewGame(r, ps)
 
@@ -10,13 +13,52 @@ func TestStateMergeFillsInCorrectly(t *testing.T) {
 	g.Board.Tiles[0][2] = HotelAmerican
 	g.Board.Tiles[0][3] = HotelAmerican
 
+	g.CurrentChainSizes[HotelLuxor] = 1
+	g.CurrentChainSizes[HotelAmerican] = 2
+
 	s := NewStateMerge(Piece{0, 1})
 
 	s.Do(g)
 
 	for i := 0; i < 4; i++ {
-		if g.Board.Tiles[0][i] != HotelAmerican {
-			t.Errorf("Piece at 0,%d is %d, should be %d", i, g.Board.Tiles[0][i], HotelAmerican)
+		if h := g.Board.Tiles[0][i]; h != HotelAmerican {
+			t.Errorf("Piece at 0,%d is %d, should be %d", i, h, HotelAmerican)
 		}
+	}
+
+	if s := g.CurrentChainSizes[HotelLuxor]; s != 0 {
+		t.Errorf("Should have 0 pieces of HotelLuxor, have %d", s)
+	}
+
+	if s := g.CurrentChainSizes[HotelAmerican]; s != 4 {
+		t.Errorf("Should have 4 pieces of HotelAmerican, have %d", s)
+	}
+}
+
+func TestStateMergeHandlesFourWayCorrectly(t *testing.T) {
+	r, ps := genGameParams()
+	g := NewGame(r, ps)
+
+	fmt.Println("-----")
+
+	g.Board.Tiles[1][0] = HotelLuxor
+	g.Board.Tiles[0][1] = HotelLuxor
+	g.Board.Tiles[0][0] = HotelLuxor
+	g.Board.Tiles[1][2] = HotelAmerican
+	g.Board.Tiles[2][1] = HotelAmerican
+
+	g.CurrentChainSizes[HotelLuxor] = 3
+	g.CurrentChainSizes[HotelAmerican] = 2
+
+	s := NewStateMerge(Piece{1, 1})
+
+	s.Do(g)
+
+	if s := g.CurrentChainSizes[HotelLuxor]; s != 6 {
+		t.Errorf("HotelLuxor should have 6, but has %d", s)
+	}
+
+	if s := g.CurrentChainSizes[HotelAmerican]; s != 0 {
+		t.Errorf("HotelAmerican should have 0, but has %d", s)
 	}
 }
